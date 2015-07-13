f49b87b8bf937ff6ccddd469a448ffd6897424a7
benjwg <benj.wg@gmail.com>
2015-07-13 13:37:56 -0400

    init creator
    updated test file - CK
    
    adding behaviors
    
    html change
    
    fix listening
    
    templates - CK
    
    added ui js base - CK

---
+++ mapper/static/js/mapper_generate.js
@@ -0,0 +1,19 @@
+Mapper.generate = new function() {
+
+	this.init = function() {
+		Mapper.map_components.map = L.mapbox.map('map', 'mapbox.streets', {
+		doubleClickZoom: false })
+    	.setView([38.1089, 13.3545], 8);
+
+    	Mapper.map_components.markerGroup = new L.LayerGroup();
+    	Mapper.generate.addListeners()
+
+	}
+
+	    this.addListeners = function() {
+	    	Mapper.map_components.map.on('dblclick', Mapper.annotate.addMarker)
+
+    }
+
+
+}();
