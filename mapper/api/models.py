f4d71165c99df7ff5d30fceb77e9c709a03b4ee0
Jane Kelly <jane.kelly@buzzfeed.com>
2015-07-13 15:26:51 -0400

    updated models

--- mapper/api/models.py
+++ mapper/api/models.py
@@ -13,8 +13,14 @@ class Map(geomodels.Model):
 class MapPoint(geomodels.Model):
 	map = models.ForeignKey(Map, related_name = 'points')
 	name = models.CharField(max_length=256)
+	notes = models.CharField(max_length=1024, blank=True)
+	timestamp = models.DateTimeField(null=True,blank=True)
 	point = geomodels.PointField()
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
 
 	objects = geomodels.GeoManager()
 
-	
+
+
+
