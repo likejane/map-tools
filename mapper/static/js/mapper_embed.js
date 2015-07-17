32664778b35b8186f0eae7a9bdebc0b42424004e
Jane Kelly <jane.kelly@buzzfeed.com>
2015-07-17 15:28:36 -0400

    freeze bpage map

--- mapper/static/js/mapper_embed.js
+++ mapper/static/js/mapper_embed.js
@@ -6,8 +6,13 @@ Mapper.embed = new function() {
         L.mapbox.accessToken = 'pk.eyJ1IjoicmFkZGRpYyIsImEiOiJkNmU0ZDNkZTg1ZmEzYzZjNGRmYWE0NDg2ODNlMzkxYyJ9.JuCOrEi6fdy9zRDW-lJo7A';
 
         if (Mapper.map_components.map) Mapper.map_components.map.remove();
-        Mapper.map_components.map = new L.mapbox.map('map', 'mapbox.streets')
-            .addControl(L.mapbox.geocoderControl('mapbox.places'));
+        Mapper.map_components.map = new L.mapbox.map('map', 'mapbox.streets',{zoomControl: false});
+
+        // Disable drag and zoom handlers.
+        Mapper.map_components.map.dragging.disable();
+        Mapper.map_components.map.touchZoom.disable();
+        Mapper.map_components.map.doubleClickZoom.disable();
+        Mapper.map_components.map.scrollWheelZoom.disable();
 
         Mapper.map_components.storageMarkerLayer = new L.geoJson()
                 .addTo(Mapper.map_components.map);
